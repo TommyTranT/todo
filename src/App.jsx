ee04919071d7a4168841b178b551cdb93d226d3c
Tommy Tran <haitran1995@gmail.com>
2022-11-09 03:52:59 -0500

    make code prettier

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import TodoItem from "./TodoItem";
 import "./App.css";
 import { useState } from "react";
 // import { MdTitle } from "react-icons/md";
-import titlepng from './title.png';
+import titlepng from "./title.png";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from './Home';
-
+import Home from "./Home";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (text, note, price, img, link) => {
-
     let id = 1;
     if (todos.length > 0) {
       id = todos[0].id + 1;
@@ -33,78 +31,70 @@ function App() {
   };
 
   const handleDelete = (id) => {
-    const newTodoes = todos.filter((el) => el.id !== id)
+    const newTodoes = todos.filter((el) => el.id !== id);
 
-    setTodos(newTodoes, ...todos)
-  }
+    setTodos(newTodoes, ...todos);
+  };
 
   const handleComplete = (id) => {
     const updateTodos = todos.map((el) => {
       if (el.id === id) {
-        el.complete = !el.complete
+        el.complete = !el.complete;
       }
-      return el
-    })
+      return el;
+    });
 
-    setTodos(updateTodos)
-  }
+    setTodos(updateTodos);
+  };
 
   const elements = todos.map((el) => (
-  <TodoItem
-    text = {el.text}
-    note = {el.note}
-    price = {el.price}
-    img = {el.img}
-    link = {el.link}
-    id = {el.id}
-    key = {el.key}
-    todo = {el}
-    handleDelete = {handleDelete}
-    handleComplete = {handleComplete}
-    complete = {el.complete}
-  />
+    <TodoItem
+      text={el.text}
+      note={el.note}
+      price={el.price}
+      img={el.img}
+      link={el.link}
+      id={el.id}
+      key={el.key}
+      todo={el}
+      handleDelete={handleDelete}
+      handleComplete={handleComplete}
+      complete={el.complete}
+    />
   ));
 
   console.log(todos);
 
   return (
     <Router>
-
-      <img className="imgTitle" src={titlepng}/>
-              <div className="homelink">    
-              <Link to="/home">Home</Link>
-              </div>
-              <div className="createlink">  
-              <Link to="/" >Create Wishlist</Link>
-              </div>
-            <Switch>
-              <Route path='/home'>
-              <Home />
-              </Route>
-              <Route path='/'>
-
-
-                
-    <div className="cont1" >
-      <div className="cont2">
-        <div className="App">
-          <div className="form">
-
-            <TodoForm addTodo={addTodo} />
-          </div>
-        </div>
+      <img className="imgTitle" src={titlepng} />
+      <div className="homelink">
+        <Link to="/home">Home</Link>
       </div>
+      <div className="createlink">
+        <Link to="/">Create Wishlist</Link>
+      </div>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/">
+          <div className="cont1">
+            <div className="cont2">
+              <div className="App">
+                <div className="form">
+                  <TodoForm addTodo={addTodo} />
+                </div>
+              </div>
+            </div>
             <div className="cont3">
-            <div className="itemForm">
-              {elements}
+              <div className="itemForm">{elements}</div>
             </div>
           </div>
-    </div>
-              </Route>
-            </Switch>
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 export default App;
-
